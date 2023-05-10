9f626f3356df94ead1e04fc741f2fc89958bb933
Sinantrarion <vikbar199@gmail.com>
2023-05-10 22:27:12 +0300

    v1 implementation

--- token-info-icons.js
+++ token-info-icons.js
@@ -1,124 +1,169 @@
 class TokenInfoIcons {
   static async addTokenInfoButtons(app, html, data) {
-      let actor = canvas.tokens.get(data._id).actor;
-      //let actor = game.actors.get(data.actorId);
-      if (actor === undefined) return;
+    let actor = canvas.tokens.get(data._id).actor;
+    //let actor = game.actors.get(data.actorId);
+    if (actor === undefined) return;
 
-      let ac = 10
-      if (game.world.system === "pf1") {
-          ac = actor.system.attributes.ac.normal.total
-      }
-      else if (game.world.system === "dcc") {
-            ac = actor.system.attributes.ac.value
-      } else {
-          ac = (isNaN(parseInt(actor.system.attributes.ac.value)) || parseInt(actor.system.attributes.ac.value) === 0) ? 10 : parseInt(actor.system.attributes.ac.value);
-      }
+    let perceptionTitle = "Passive Perception";
+    let perception = 10;
+    let insight = 10;
+    let esp = 10;
+    let od = 10;
+    // cus_2 cus_4 cus_7
+    insight = actor.system.skills.cus_2.passive;
+    perception = actor.system.skills.prc.passive;
+    esp = actor.system.skills.cus_4.passive
+    od = actor.system.skills.cus_7.passive
 
-      let perceptionTitle = "Passive Perception";
-      let perception = 10;
-      if (game.world.system === "pf1") {
-          perception = actor.system.skills.per.mod
-          perceptionTitle = "Perception Mod";
-      } else if (game.world.system === "pf2e") {
-          perception = perception + actor.system.attributes.perception.value;
-          perceptionTitle = "Perception DC";
-      }
-      else if (game.world.system === "dcc") {
-        perception = 0
-        perceptionTitle = "Perception DC";
-      }
-       else {
-          perception = actor.system.skills.prc.passive;
-      }
-
-      //console.log("TokenInfoIcons", actor);
-
-      let speed = "";
-
-      if (game.world.system === "pf2e") {
-          if (actor.data.type === "npc") {
-              speed = '<span class="token-info-speed" title="Speed"><i class="fas fa-walking"></i><span style="font-size: 0.65em;"> ' + actor.system.attributes.speed.value + '</span></span>';
-          } else if (actor.data.type === "familiar") {
-              // Familiars seem to get either 25 ft. land or water speed
-              // It can be modified by other abilities but they will be revising these later so this will likely change
-              speed = '<span class="token-info-speed" title="Speed"><i class="fas fa-walking"></i> 25</span>';
-          } else {
-              speed = '<span class="token-info-speed" title="Land"><i class="fas fa-walking"></i> ' + actor.system.attributes.speed.total + '</span>';
-          }
-      } else if (game.world.system === "pf1") {
-          speed = '<span class="token-info-speed" title="Land"><i class="fas fa-walking"></i> ' + actor.system.attributes.speed.land.total + '</span>';
-      } else if (game.world.system === "dcc") {
-          speed = '<span class="token-info-speed" title="Movement"><i class="fas fa-walking"></i> ' + actor.system.attributes.speed.base + '</span>';
-      } else {
-          if (actor.system.attributes.movement.walk != 0 && actor.system.attributes.movement.walk != null) speed += '<span class="token-info-speed" title="Walk"><i class="fas fa-walking"></i> ' + actor.system.attributes.movement.walk + '<span style="font-size: 0.5em;"> ' + actor.system.attributes.movement.units + "</span></span>";
-          if (actor.system.attributes.movement.swim != 0 && actor.system.attributes.movement.swim != null) speed += '<span class="token-info-speed" title="Swim"><i class="fas fa-swimmer"></i> ' + actor.system.attributes.movement.swim + '<span style="font-size: 0.5em;"> ' + actor.system.attributes.movement.units + "</span></span>";
-          if (actor.system.attributes.movement.fly != 0 && actor.system.attributes.movement.fly != null) speed += '<span class="token-info-speed" title="Fly"><i class="fas fa-crow"></i> ' + actor.system.attributes.movement.fly + '<span style="font-size: 0.5em;"> ' + actor.system.attributes.movement.units + "</span></span>";
-          if (actor.system.attributes.movement.burrow != 0 && actor.system.attributes.movement.burrow != null) speed += '<span class="token-info-speed" title="Burrow"><i class="fas fa-mountain"></i> ' + actor.system.attributes.movement.burrow + '<span style="font-size: 0.5em;"> ' + actor.system.attributes.movement.units + "</span></span>";
-          if (actor.system.attributes.movement.climb != 0 && actor.system.attributes.movement.climb != null) speed += '<span class="token-info-speed" title="Climb"><i class="fas fa-grip-lines"></i> ' + actor.system.attributes.movement.climb + '<span style="font-size: 0.5em;"> ' + actor.system.attributes.movement.units + "</span></span>";
-      }
-
-      // DCC luck
-
-      let luck = null;
-      if (game.world.system === "dcc") {
-        if (actor.data.type === "Player") {
-          luck =  actor.system.abilities.lck.value;
-        }
-      }
+    //console.log("TokenInfoIcons", actor);
 
       let newdiv = '<div class="token-info-container">';
 
-      let position = game.settings.get('token-info-icons', 'position');
+      let position = game.settings.get('token-info-icons-project-m', 'position');
 
-      let defaultButtons = '<div class="control-icon token-info-icon">' + speed + '</div><div class="control-icon token-info-icon" title="Armor Class: ' + ac + '"><i class="fas fa-shield-alt"></i> ' + ac + '</div>';
-      if (game.world.system !== "dcc"){
-        defaultButtons += '<div class="control-icon token-info-icon" title="Passive Perception: ' + perception + '"><i class="fas fa-eye"></i> ' + perception + '</div>'
-      }else{
-        // dcc specific
-        if(luck != null){
-          defaultButtons += '<div class="control-icon token-info-icon" title="Luck: ' + luck + '"><i class="fas fa-star"></i> ' + luck + '</div>'
-        }
-      }
-      
+      let defaultButtons = '<div class="control-icon token-info-icon" title="Insight: ' + insight + '"><i class="fas fa-genderless"></i> ' + insight + '</div>';
+      defaultButtons += '<div class="control-icon token-info-icon" title="ESP: ' + esp + '"><i class="fas fa-eye-slash"></i> ' + esp + '</div>';
+      defaultButtons += '<div class="control-icon token-info-icon" title="Odicism: ' + od + '"><i class="fas fa-heart-circle-exclamation"></i> ' + od + '</div>';
+      defaultButtons += '<div class="control-icon token-info-icon" title="Perception: ' + perception + '"><i class="fas fa-eye"></i> ' + perception + '</div>';
 
       let passiveSensesButtons = '';
-      if (!['pf2e', 'pf1'].includes(game.world.system) && game.settings.get('token-info-icons', 'allPassiveSenses')) {
-          const investigation = actor.system.skills.inv.passive;
-          const insight = actor.system.skills.ins.passive;
-          const stealth = actor.system.skills.ste.passive;
-
-          const passiveInvestigationButton = `<div class="control-icon token-info-icon" title="Passive Investigation: ${investigation}"><i class="fas fa-search"></i> ${investigation}</div>`;
-          const passiveInsightButton = `<div class="control-icon token-info-icon" title="Passive Insight: ${insight}"><i class="fas fa-lightbulb"></i> ${insight}</div>`;
-          const passiveStealthButton = `<div class="control-icon token-info-icon" title="Passive Stealth: ${stealth}"><i class="fas fa-eye-slash"></i> ${stealth}</div>`;
-          passiveSensesButtons = `${passiveInvestigationButton}${passiveInsightButton}${passiveStealthButton}`;
-      }
+      
 
       let buttons = $(`<div class="col token-info-column-${position}">${defaultButtons}${passiveSensesButtons}</div>`);
 
       html.find('.col.left').wrap(newdiv);
       html.find('.col.left').before(buttons);
   }
+
+    static async AddWeaponProperties()
+    {
+        CONFIG.DND5E.weaponProperties['armorpiercing'] = 'Armor Piercing';
+        CONFIG.DND5E.weaponProperties['brutal'] = 'Brutal';
+        CONFIG.DND5E.weaponProperties['cleave'] = 'Cleave';
+        CONFIG.DND5E.weaponProperties['concealable'] = 'Concealable';
+        CONFIG.DND5E.weaponProperties['damage'] = 'Damage';
+        CONFIG.DND5E.weaponProperties['fist'] = 'Fist';
+        CONFIG.DND5E.weaponProperties['hafted'] = 'Hafted';
+        CONFIG.DND5E.weaponProperties['momentum'] = 'Momentum';
+        CONFIG.DND5E.weaponProperties['precision'] = 'Precision';
+        CONFIG.DND5E.weaponProperties['swift'] = 'Swift';
+        CONFIG.DND5E.weaponProperties['trip'] = 'Trip';
+
+        CONFIG.DND5E.abilityActivationTypes['free'] = 'Free Action';
+
+        CONFIG.DND5E.armorClasses.unarmoredBarb.formula = "10 + @abilities.str.mod + @abilities.con.mod";
+
+        CONFIG.DND5E.sourcePacks['EXTRA'] = "token-info-icons-project-m.dndplus";
+
+        CONFIG.DND5E.weaponIds = [];
+        CONFIG.DND5E.weaponIds['cestus'] = "token-info-icons-project-m.dndplus.LRz1dDN3Ic3Qw4uk";
+        CONFIG.DND5E.weaponIds['clawgauntlet'] = "token-info-icons-project-m.dndplus.X2FZBx2YR5twA0im";
+        CONFIG.DND5E.weaponIds['club'] = "token-info-icons-project-m.dndplus.2aaGVYy2wQq6oYic";
+        CONFIG.DND5E.weaponIds['dagger'] = "token-info-icons-project-m.dndplus.TmZB6OZEDEYUhzzj";
+        CONFIG.DND5E.weaponIds['greatclub'] = "token-info-icons-project-m.dndplus.PwTZ2KVj4g2M0i5v";
+        CONFIG.DND5E.weaponIds['handaxe'] = "token-info-icons-project-m.dndplus.in8S2YR8Tlg9Tfor";
+        CONFIG.DND5E.weaponIds['lighthammer'] = "token-info-icons-project-m.dndplus.uZghiR52kzdUVOvi";
+        CONFIG.DND5E.weaponIds['parryingdagger'] = "token-info-icons-project-m.dndplus.doTqc5HSqGDtX8oD";
+        CONFIG.DND5E.weaponIds['pickaxe'] = "token-info-icons-project-m.dndplus.rQaUjy5wpdp7erkx";
+        CONFIG.DND5E.weaponIds['punchingdagger'] = "token-info-icons-project-m.dndplus.JCgsVLgQQZSADv3S";
+        CONFIG.DND5E.weaponIds['quarterstaff'] = "token-info-icons-project-m.dndplus.Ro2EqQRSA4HWyGgI";
+        CONFIG.DND5E.weaponIds['sickle'] = "token-info-icons-project-m.dndplus.pK24pDnl6j9NnnIc";
+        CONFIG.DND5E.weaponIds['spear'] = "token-info-icons-project-m.dndplus.uxF19CgGoQmtyd0s";
+        CONFIG.DND5E.weaponIds['lightcrossbow'] = "token-info-icons-project-m.dndplus.tNGVF31u9gkk943U";
+        CONFIG.DND5E.weaponIds['net'] = "token-info-icons-project-m.dndplus.t3GKRKLMMiM7XLAr";
+        CONFIG.DND5E.weaponIds['shortbow'] = "token-info-icons-project-m.dndplus.78makCHJNmZwdPQe";
+        CONFIG.DND5E.weaponIds['shuriken'] = "token-info-icons-project-m.dndplus.RLQtXuyVYHeGFnDM";
+        CONFIG.DND5E.weaponIds['sling'] = "token-info-icons-project-m.dndplus.nS92E77UwSWtg6Q4";
+
+        CONFIG.DND5E.weaponIds['doublesword'] = "token-info-icons-project-m.dndplus.GOdeMWi46FA8jSzm";
+        CONFIG.DND5E.weaponIds['flail'] = "token-info-icons-project-m.dndplus.HzV2aVCp2MKRtjbi";
+        CONFIG.DND5E.weaponIds['greataxe'] = "token-info-icons-project-m.dndplus.291yMI546pvuiH1f";
+        CONFIG.DND5E.weaponIds['halberd'] = "token-info-icons-project-m.dndplus.a6f3qFxsLUkoRiXJ";
+        CONFIG.DND5E.weaponIds['lance'] = "token-info-icons-project-m.dndplus.LBbKpm5qoXwJIkTj";
+        CONFIG.DND5E.weaponIds['longsword'] = "token-info-icons-project-m.dndplus.SqnzJoxjvVlfU55E";
+        CONFIG.DND5E.weaponIds['lucerne'] = "token-info-icons-project-m.dndplus.zwukKQnAsA7sfHbg";
+        CONFIG.DND5E.weaponIds['mace'] = "token-info-icons-project-m.dndplus.afIn0HqHGTbyUxPh";
+        CONFIG.DND5E.weaponIds['maul'] = "token-info-icons-project-m.dndplus.zMLpgd3g3XGfe298";
+        CONFIG.DND5E.weaponIds['morningstarwhip'] = "token-info-icons-project-m.dndplus.jpdnHJBCgnsNjXiN";
+        CONFIG.DND5E.weaponIds['nunchaku'] = "token-info-icons-project-m.dndplus.jsZuARa4YaQAIe9I";
+        CONFIG.DND5E.weaponIds['pike'] = "token-info-icons-project-m.dndplus.17mveT9h8We1oHy0";
+        CONFIG.DND5E.weaponIds['rapier'] = "token-info-icons-project-m.dndplus.zGYicwdrotOWVjB0";
+        CONFIG.DND5E.weaponIds['sabre'] = "token-info-icons-project-m.dndplus.QbM0Kup4knZsQmEM";
+        CONFIG.DND5E.weaponIds['scythe'] = "token-info-icons-project-m.dndplus.WxCGQerY2O84AbCy";
+        CONFIG.DND5E.weaponIds['shortsword'] = "token-info-icons-project-m.dndplus.JS8fbXRTqv9Xk67B";
+        CONFIG.DND5E.weaponIds['trident'] = "token-info-icons-project-m.dndplus.7P04AiYZaszTRMVH";
+        CONFIG.DND5E.weaponIds['warmace'] = "token-info-icons-project-m.dndplus.uJ0w6VFsYARJJnod";
+        CONFIG.DND5E.weaponIds['warpick'] = "token-info-icons-project-m.dndplus.7xbTPYy5g1BIpDRW";
+        CONFIG.DND5E.weaponIds['warhammer'] = "token-info-icons-project-m.dndplus.X6ClFpIceSXs2hYi";
+        CONFIG.DND5E.weaponIds['whip'] = "token-info-icons-project-m.dndplus.GToXh5ARWMGa6cI2";
+        CONFIG.DND5E.weaponIds['zweihander'] = "token-info-icons-project-m.dndplus.7JibDfaNCoJwMe5Q";
+        CONFIG.DND5E.weaponIds['arbalest'] = "token-info-icons-project-m.dndplus.7C6J84FKGEKGYhs2";
+        CONFIG.DND5E.weaponIds['blowgun'] = "token-info-icons-project-m.dndplus.Hm7Hn64P2HKYpEHM";
+        CONFIG.DND5E.weaponIds['bola'] = "token-info-icons-project-m.dndplus.v0GnLAxzCBnnnChf";
+        CONFIG.DND5E.weaponIds['chakram'] = "token-info-icons-project-m.dndplus.lf6DDxH99He3tu3Z";
+        CONFIG.DND5E.weaponIds['greatbow'] = "token-info-icons-project-m.dndplus.b4IwUzr2coSEKYTj";
+        CONFIG.DND5E.weaponIds['handcrossbow'] = "token-info-icons-project-m.dndplus.pk7xbWxrrinpzbzE";
+        CONFIG.DND5E.weaponIds['longbow'] = "token-info-icons-project-m.dndplus.44hSQ1wS1aOlUxWd";
+        CONFIG.DND5E.weaponIds['repeatingcrossbow'] = "token-info-icons-project-m.dndplus.upJlvfjsF0soS4Yd";
+
+        CONFIG.DND5E.armorIds = [];
+        CONFIG.DND5E.armorIds['fullplate'] = "token-info-icons-project-m.dndplus.v8GQvGwwzvoHsOPR";
+        CONFIG.DND5E.armorIds['lamellar'] = "token-info-icons-project-m.dndplus.mzBlFSwjSAeL0pzs";
+        CONFIG.DND5E.armorIds['splint'] = "token-info-icons-project-m.dndplus.9a5UmSNn1Bs57TPT";
+        CONFIG.DND5E.armorIds['brigandine'] = "token-info-icons-project-m.dndplus.XFYaCoJpPjTcwtKd";
+        CONFIG.DND5E.armorIds['studdedleather'] = "token-info-icons-project-m.dndplus.b90zsDrcghVN3fNt";
+        CONFIG.DND5E.armorIds['banded'] = "token-info-icons-project-m.dndplus.69EcxRqpejDDuDi6";
+        CONFIG.DND5E.armorIds['halfplate'] = "token-info-icons-project-m.dndplus.unk29aZMF6theiGe";
+        CONFIG.DND5E.armorIds['scalemail'] = "token-info-icons-project-m.dndplus.aJOce6sLA346QANx";
+        CONFIG.DND5E.armorIds['woodenplate'] = "token-info-icons-project-m.dndplus.zMdjOt0ROaSkh64p";
+
+        CONFIG.DND5E.armorIds['breastplate'] = "token-info-icons-project-m.dndplus.rSw4KC5kv7OC1raE";
+        CONFIG.DND5E.armorIds['chainmail'] = "token-info-icons-project-m.dndplus.p0FLNjv7O8zWqrTv";
+        CONFIG.DND5E.armorIds['ringmail'] = "token-info-icons-project-m.dndplus.OPiKXGWy0MZilHk2";
+        CONFIG.DND5E.armorIds['leather'] = "token-info-icons-project-m.dndplus.lACEDmxYzExKT4E1";
+        CONFIG.DND5E.armorIds['padded'] = "token-info-icons-project-m.dndplus.qRxf5XRvr598IhbA";
+        CONFIG.DND5E.armorIds['chainshirt'] = "token-info-icons-project-m.dndplus.A1gqMK6VU8rS2EjE";
+        CONFIG.DND5E.armorIds['hidearmor'] = "token-info-icons-project-m.dndplus.4gZC1krxPjktEMsU";
+
+        CONFIG.DND5E.shieldIds = [];
+        CONFIG.DND5E.shieldIds['buckler'] = "token-info-icons-project-m.dndplus.fEUsUOoSebaEG8NV";
+        CONFIG.DND5E.shieldIds['heater'] = "token-info-icons-project-m.dndplus.IEnW7C8Zvyf40k7O";
+        CONFIG.DND5E.shieldIds['kite'] = "token-info-icons-project-m.dndplus.OuKBqSgupvE5miXK";
+        CONFIG.DND5E.shieldIds['tower'] = "token-info-icons-project-m.dndplus.XqZT5LCnxaIEJQTe";
+
+        CONFIG.DND5E.languages = [];
+        CONFIG.DND5E.languages['esp'] = "Esperanto";
+        CONFIG.DND5E.languages['eng'] = "English";
+        CONFIG.DND5E.languages['spa'] = "Spanish";
+        CONFIG.DND5E.languages['ger'] = "German";
+        CONFIG.DND5E.languages['fre'] = "French";
+        CONFIG.DND5E.languages['pol'] = "Polish";
+        CONFIG.DND5E.languages['ukr'] = "Ukranian";
+        CONFIG.DND5E.languages['dut'] = "Dutch";
+        CONFIG.DND5E.languages['ita'] = "Italian";
+        CONFIG.DND5E.languages['por'] = "Portugese";
+
+    }
 }
 
-Hooks.on('ready', () => {
-  const gmOnly = game.settings.get('token-info-icons', 'gmOnly');
 
-  if (gmOnly) {
-      if (game.user.isGM) {
-          Hooks.on('renderTokenHUD', (app, html, data) => {
-              TokenInfoIcons.addTokenInfoButtons(app, html, data)
-          });
-      }
-  } else {
-      Hooks.on('renderTokenHUD', (app, html, data) => {
-          TokenInfoIcons.addTokenInfoButtons(app, html, data)
-      });
-  }
+
+Hooks.on('ready', () => {
+
+    TokenInfoIcons.AddWeaponProperties();
+
+  //const gmOnly = game.settings.get('token-info-icons-project-m', 'gmOnly');
+
+  
+    Hooks.on('renderTokenHUD', (app, html, data) => {
+        TokenInfoIcons.addTokenInfoButtons(app, html, data)
+    });
+
 });
 
 Hooks.once("init", () => {
 
-  game.settings.register('token-info-icons', 'gmOnly', {
+  game.settings.register('token-info-icons-project-m', 'gmOnly', {
       name: "GM only?",
       hint: "Show the token info to the GM only or to all players?",
       scope: "world",
@@ -127,16 +172,7 @@ Hooks.once("init", () => {
       type: Boolean
   });
 
-  game.settings.register('token-info-icons', 'allPassiveSenses', {
-      name: 'Show all passive senses (dnd5e)',
-      hint: 'Show passive perception/investigation/insight/stealth instead of just passive perception',
-      scope: "world",
-      config: true,
-      default: false,
-      type: Boolean
-  });
-
-  game.settings.register('token-info-icons', 'position', {
+  game.settings.register('token-info-icons-project-m', 'position', {
       name: "Token Position",
       hint: "Which side of the token should the info appear on?",
       scope: "world",
